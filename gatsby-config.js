d1542285ab77cd43dda5ab201f99e59c03f48bfe
wykhuh <wykhuh@users.noreply.github.com>
2019-06-16 05:42:40 -0700

    connect to onegraph

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,9 +1,13 @@
-/**
- * Configure your Gatsby site with this file.
- *
- * See: https://www.gatsbyjs.org/docs/gatsby-config/
- */
-
 module.exports = {
-  /* Your site config here */
+  plugins: [
+    {
+      resolve: "gatsby-source-graphql",
+      options: {
+        typeName: "ONEGRAPH",
+        fieldName: "oneGraph",
+        url:
+          "https://serve.onegraph.com/dynamic?app_id=cf005b0f-864d-49b2-9b61-9404d3d8962c",
+      },
+    },
+  ],
 }
---
+++ z_notes/notes.md
@@ -0,0 +1,13 @@
+Build a Podcast Mashup App Using OneGraph and Gatsby — Learn With Jason
+https://www.youtube.com/watch?v=10jeoEWy-8g
+
+https://github.com/jlengstorf/podcast-mashup
+
+==
+
+gatsby new podcast-mashup https://github.com/gatsbyjs/gatsby-starter-hello-world
+
+==
+gatsby-source-graphql
+
+- connect to any graphql api
